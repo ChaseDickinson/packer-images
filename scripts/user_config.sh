28fc2f175e50a7d51a4dfdc47bb8935d2628a3c8
Chase <chase.dickinson@gmail.com>
2019-11-11 20:21:20 -0600

    checking in to test

--- scripts/user_config.sh
+++ scripts/user_config.sh
@@ -8,16 +8,19 @@ echo -e '\n ... Configuring favorites ... \n'
 gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Terminal.desktop', 'code_code.desktop', 'org.gnome.gedit.desktop', 'org.gnome.Nautilus.desktop', 'update-manager.desktop', 'gnome-control-center.desktop']"
 
 #install code extensions
-#code --install-extension amazonwebservices.aws-toolkit-vscode
-#code --install-extension equinusocio.vsc-material-theme
-#code --install-extension pkief.material-icon-theme
-#code --install-extension ms-azuretools.vscode-docker
+code --install-extension amazonwebservices.aws-toolkit-vscode
+code --install-extension equinusocio.vsc-material-theme
+code --install-extension pkief.material-icon-theme
+code --install-extension ms-azuretools.vscode-docker
+
+#pause before configuring settings
+sleep 10
 
 #vs code settings
-#echo 'export PATH=~/.local/bin:$PATH' >>~/.bashrc
-#echo 'export "workbench.colorTheme": "Material Theme Darker"' >>~/.config/Code/User/settings.json
-#echo 'export "materialTheme.accent": "Orange"'  >>~/.config/Code/User/settings.json
+echo -e '\t"workbench.colorTheme": "Material Theme Darker"' >>~/.config/Code/User/settings.json
+echo -e '\t"materialTheme.accent": "Orange"'  >>~/.config/Code/User/settings.json
+echo -e '\t"workbench.iconTheme": "material-icon-theme"'  >>~/.config/Code/User/settings.json
 
 #forcing user to change password
-#echo -e '\n ... Requiring user to set a new password at next login ... \n'
-#echo $PASSWORD | sudo -S charge --lastday 0 ubuntu
+echo -e '\n ... Setting Password to Expire in a Day ... \n'
+echo $PASSWORD | sudo -S chage -M 1 $USERNAME
