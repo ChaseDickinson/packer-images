09e05578e6629b92c3dadeaf6ac41796cc1f00b5
Chase Dickinson <chase.dickinson@gmail.com>
2019-12-22 22:02:32 -0600

    updating linux_vm_tools script for bionic

--- scripts/linux_vm_tools.sh
+++ scripts/linux_vm_tools.sh
@@ -3,6 +3,14 @@
 #pause before beginning install
 sleep 10
 
+#install prerequisites for bionic
+if [ "$OS_NICKNAME" == "bionic" ];
+then
+    echo -e '\n ... Installing prerequirements for Bionic ... \n'
+    echo $PASSWORD | sudo -S apt-get install -y xorg-video-abi-23 xserver-xorg-core
+    echo $PASSWORD | sudo -S apt-get install -y xorgxrdp
+fi
+
 #microsoft linux-vm-tools
 echo -e '\n ... Installing Microsoft Linux-VM-Tools ... \n'
 wget https://raw.githubusercontent.com/Microsoft/linux-vm-tools/master/ubuntu/18.04/install.sh
