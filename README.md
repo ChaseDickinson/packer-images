eab4fd24d4fb12caae60d6c72301ec21004009a0
Chase Dickinson <chase.dickinson@gmail.com>
2019-12-29 15:32:02 -0600

    updating password workflow

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 ## TODO
 
-1) Add Virtualbox builder
-2) Terminal theming - font & Powerline install, gsettings commands
-3) aws cli v2
-4) Error handling in password change script
+1) Password change workflow - replace initial setup file
+2) Add Virtualbox builder
+3) Terminal theming - font & Powerline install, gsettings commands
+4) aws cli v2
 5) Adopt HCL2
 
 ## Problem Statement
--- files/passwd.sh
+++ files/passwd.sh
@@ -6,15 +6,28 @@ read -sp "Please enter current password for username $username: " current_passwo
 
 echo " "
 
+password_check () {
+    echo $current_password | sudo -S apt-get update &> /dev/null
+    echo $?
+}
+
 password_change () {
-    new_password=$(openssl rand -base64 12)
+    new_password=$(openssl rand -base64 10)
 
     echo -e "$current_password\n$new_password\n$new_password" | passwd &> /dev/null
 
-    message="New password for user $username is: \n$new_password"
+    message="New password for user $username is: \n$new_password\n"
     echo -e $message
 }
 
-output=$(password_change)
+correct_password=$(password_check)
 
-echo -e "$output"
+if [ $correct_password -eq 0 ]
+then
+    output=$(password_change)
+    echo -e "$output"
+    #this command closes the window before the new password can be captured [I'm guessing?]
+    #echo -n "yes" > ~/.config/password-reset-done
+else
+    echo "Incorrect password entered. Reboot to try again."
+fi
