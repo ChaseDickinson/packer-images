60c63aa2c7892f98b84ee114c6037c3c5297aa3d
Chase <chase.dickinson@gmail.com>
2020-11-06 10:45:04 -0600

    correcting passwd.sh logic

--- files/passwd.sh
+++ files/passwd.sh
@@ -8,24 +8,40 @@ set -o errtrace
 set -o nounset
 
 password_check () {
-    current_shadow=$(echo "${current_password}" | sudo -S cat /etc/shadow 2>/dev/null | grep -Po '(?<='"${username}"':)(.*?)(?=:)')
+    hash_current=$(echo "${current_password}" | sudo -S cat /etc/shadow 2>/dev/null | grep "$username" | awk -F: '{print $2}')
+    IFS='$'
+    read -ra arr_hash_current <<< "${hash_current}"
+    unset IFS
 
-    if [[ -z "${current_shadow}" ]]
+    #debug info
+    #echo "arr_hash_current length:"
+    #echo "${#arr_hash_current[@]}"
+    #echo "arr_hash_current values:"
+    #printf '%s\n' "${arr_hash_current[@]}"
+
+    if [ "${#arr_hash_current[*]}" -gt 0 ]
     then
-      echo "Error: Incorrect password entered for ${username}."
-      exit 1
-    else
-      current_algorithm=$(echo "${current_password}" | sudo -S cat /etc/shadow | grep -Po '(?<='"${username}"':\$)(\d)')
-      current_salt=$(echo "${current_password}" | sudo -S cat /etc/shadow | grep -Po '(?<='"${username}"':\$\d\$)(.+?)(?=\$)')
-      validate_shadow=$(echo "${current_password}" | openssl passwd -stdin -"${current_algorithm}" -salt "${current_salt}")
+        hash_validate=$(echo "${current_password}" | openssl passwd -stdin -"${arr_hash_current[1]}" -salt "${arr_hash_current[2]}")
+        IFS='$'
+        read -ra arr_hash_validate <<< "${hash_validate}"
+        unset IFS
 
-      if [[ "${validate_shadow}" == "${current_shadow}" ]]
-      then
-          validated=true
-      else
-          echo "Error: Hashes do not match."
-          exit 1
-      fi
+        #debug info
+        #echo "arr_hash_validate length:"
+        #echo "${#arr_hash_validate[@]}"
+        #echo "arr_hash_validate values:"
+        #printf '%s\n' "${arr_hash_validate[@]}"
+
+        if [ "${arr_hash_validate[3]}" == "${arr_hash_current[3]}" ]
+        then
+            validated=true
+        else
+            echo "Error: Hashes do not match."
+            exit 1
+        fi
+    else
+        echo "Error: Incorrect password entered for $username."
+        exit 1
     fi
 }
 
@@ -41,16 +57,16 @@ password_change () {
 main () {
     username=$(whoami)
 
-    read -rsp "Please enter current password for username ${username}: " current_password
+    read -rsp "Please enter current password for username $username: " current_password
 
     echo " "
 
     password_check
 
-    if [ "${validated}" ] && [ "${#validate_shadow}" -gt 0 ]
+    if [ "$validated" ] && [ "${#arr_hash_current[*]}" -gt 0 ]
     then
         output=$(password_change)
-        echo -e "${output}"
+        echo -e "$output"
         read -rsp $'Once you\'ve saved the new password, press ENTER to continue.\n'
         echo -n "yes" > ~/.config/password-reset-done
     else
